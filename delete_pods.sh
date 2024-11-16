8608f3d480856c94f1b4d97543df48a98d233f4e
V.Derina <hp@ya.com>
2024-11-16 22:28:49 +0500

    add file

--- delete_pods.sh
+++ delete_pods.sh
@@ -7,6 +7,4 @@ POD_NAME_PREFIX="my-pod"
 PODS=$(kubectl get pods -o jsonpath="{.items[*].metadata.name}" | tr ' ' '\n' | grep "^${POD_NAME_PREFIX}")
 
 # Удаляем все найденные поды
-for POD in $PODS; do
-  kubectl delete pod $POD
-done
+kubectl delete pods $PODS
