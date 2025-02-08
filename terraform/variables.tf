f6d444c8c08a5bc6d8c4ebba5a4e78985e13f889
Vlada Derina <V.Derina@compassplus.com>
2025-02-08 19:50:00 +0500

    Add README.md

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,19 +1,16 @@
 variable "postgres_host" {
   description = "PostgreSQL host"
   type        = string
-  default     = "192.168.39.24"
 }
 
 variable "postgres_port" {
   description = "PostgreSQL port"
   type        = number
-  default     = 30000
 }
 
 variable "postgres_user" {
   description = "PostgreSQL admin username"
   type        = string
-  default     = "postgres"
 }
 
 variable "postgres_password" {
